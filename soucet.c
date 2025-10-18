0439e250414d35d5d4aa1023cb7fd4778a618403
edumach <59918768+edumach@users.noreply.github.com>
2025-10-18 11:22:59 +0200

    Add soucet.c to sum two integers from arguments

---
+++ soucet.c
@@ -0,0 +1,19 @@
+################
+#   soucet.c   #
+################
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a;
+    int b;
+    
+    a = atoi(argv[1]); /* prvni argument */
+    b = atoi(argv[2]); /* druhy argument */
+
+    printf("%d + %d = %d\n", a, b, a + b);
+    
+    return 0;
+}
